45156469aff8c88277b7538b0e67d37948aa15e1
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2023-07-07 17:39:50 -0400

    Co-authored-by: Keheck <Keheck@users.noreply.github.com>

--- Assets/Scripts/Prototype/Terminal.cs
+++ Assets/Scripts/Prototype/Terminal.cs
@@ -8,23 +8,35 @@ using Cysharp.Threading.Tasks;
 public class Terminal : MonoBehaviour {
 
     TMP_InputField inputField;
-    string lastInput;
+    int lastInputPostition = 0;
 
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         inputField = GetComponent<TMP_InputField>();
-        inputField.Select();
-        inputField.ActivateInputField();
+        lastInputPostition = inputField.text.Length;
+        Reselect();
     }
 
     public void TakeInput(string input) {
-        lastInput = input;
+        if (input[input.Length-1] != '\n') input += '\n'; // ensure the line ends with a linebreak
+        string command = "";
+        if (input.Length - 1 > lastInputPostition) {
+            command = input.Substring(lastInputPostition);
+        }
+        // TODO: pass in the command to the task manager or something to get a return value
+        print(command);
+        // TODO: add return from the players command, then another linebreak
+        inputField.text += ">";
+        lastInputPostition = inputField.text.Length + 1;
         Reselect();
     }
 
     // called when deselected by user, to force you back in
+    // also called when you take input, since it automatically deselects
     public async void Reselect() {
         await UniTask.Yield();
         inputField.Select();
+        inputField.ActivateInputField();
+        inputField.MoveTextEnd(false);
     }
 }
